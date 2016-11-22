d791f740bea342f4be02ed0bf3b79e0b356f5302
SAADA_Yoav <yoavs@hotmail.fr>
2016-11-22 11:46:02 +0100

    -a

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   def index
   end
 
+  def first_page
+  end
+
   def about
   end
 
   def mon_cv
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
     root to: "games#index"
   end
 
-  root 'pages#index'
+  root 'pages#first_page'
   get 'about' => 'pages#about'
   get 'games' => 'games#index'
   get 'mon_cv' => 'pages#mon_cv'
+  get 'index' => 'pages#index'
   get 'contact' => 'pages#contact' 
   get 'games/:slug' => 'games#show', as: :game
   get 'games/:slug_game/videos/:slug_video' => 'videos#show', as: :video
